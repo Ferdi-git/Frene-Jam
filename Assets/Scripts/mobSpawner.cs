a477e6610a488f27d5ccb4693113412875a269a9
Mourette Ferdinand <mouretteferdinand@yahoo.com>
2025-12-20 23:13:13 +0100

    Wave

--- Assets/Scripts/mobSpawner.cs
+++ Assets/Scripts/mobSpawner.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 public class mobSpawner : MonoBehaviour
 {
     public GameObject mobPrefab;
-    public SOEnemy[] difEnemies;
+    public Vague[] difVagues;
     public float inBetweenSpawnTime = 0.5f;
     private bool canSpawn = true;  
+    private int curentWave = 0;
 
     void FixedUpdate()
     {
@@ -18,10 +19,10 @@ public class mobSpawner : MonoBehaviour
         canSpawn = false;
         float randfloat = Random.Range(-8f, 8f);
         Vector2 pos = new Vector2(transform.position.x + randfloat, transform.position.y);
-        int randint = Random.Range(0, difEnemies.Length);
+        int randint = Random.Range(0, difVagues[curentWave].enemies.Length);
         var newEnemy = Instantiate(mobPrefab, pos, Quaternion.identity);
-        newEnemy.GetComponent<Enemy>().Initialise(difEnemies[randint]);
-        yield return new WaitForSeconds(inBetweenSpawnTime);
+        newEnemy.GetComponent<Enemy>().Initialise(difVagues[curentWave].enemies[randint]);
+        yield return new WaitForSeconds(difVagues[curentWave].spawnInterval);
         canSpawn = true;
 
 
